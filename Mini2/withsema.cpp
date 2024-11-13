677c04ceb8a1722eb721796d360287476612e870
ijcor04 <156957236+ijcor04@users.noreply.github.com>
2024-11-12 19:39:17 -0600

    Update withsema.cpp

--- Mini2/withsema.cpp
+++ Mini2/withsema.cpp
@@ -1,1 +1,69 @@
+//Isaiah Corrales
+//Mini Project 2, CMPS-4103-101
+//Ocotber 28th, 2024
+//This program will utilize three threads to add to our global array and TOTAL. The value we get from 
+//adding our array value to the value that is already in our TOTAL global variable will be added into TOTAL.
+//The addition of values into TOTAL will also be regulated/protected by a semphore.
+
+#include <iostream>
+#include <pthread.h>
+#include <semaphore.h>
+//defining size of array and the number of threads
+#define SIZE 7500
+#define NUM_THREADS 3
+//initializing our global array & global variable
+int array[SIZE];
+long int TOTAL = 0;
+sem_t semaphore;
+
+// Function each thread will execute
+void* add_to_total(void* thread_id) 
+{
+    int tid = *(int*)thread_id;
+    int start = (tid - 1) * (SIZE / NUM_THREADS);
+    int end = tid * (SIZE / NUM_THREADS);
+
+    for (int i = start; i < end; ++i) 
+	{
+        sem_wait(&semaphore);   // Lock access to TOTAL
+        TOTAL =(TOTAL + array[i]);
+        sem_post(&semaphore);   // Unlock access to TOTAL
+    }
+
+    pthread_exit(nullptr);
+}
+
+int main() 
+{
+    
+    for (int i = 0; i < SIZE; ++i) 
+	{
+        array[i] = i + 1;
+    }
+
+    // Initialize semaphore
+    sem_init(&semaphore, 0,1);
+
+    // Create threads
+    pthread_t threads[NUM_THREADS];
+	int thread_ids[NUM_THREADS] = {1, 2, 3};
+    
+	for (int i = 0; i < NUM_THREADS; ++i) 
+    {
+        pthread_create(&threads[i], nullptr, add_to_total, (void*)&thread_ids[i]);
+    }
+
+    // Wait for all threads to complete
+    for (int i = 0; i < NUM_THREADS; ++i) 
+	{
+        pthread_join(threads[i], nullptr);
+    }
+
+    // Print TOTAL
+    std::cout << "TOTAL with semaphores: " << TOTAL << std::endl;
+	
+	// Destroy semaphore
+    sem_destroy(&semaphore);
+    return 0;
+}
 
